f4c8dca48fe63d4833c99e2c97e3632695a769d3
Clay Paris <clayp@rightline.com>
2017-10-19 17:11:06 -0700

    Add base UI toolbar and drop new tower on screen click

---
+++ Tower.js
@@ -0,0 +1,22 @@
+class Tower {
+  constructor (game, phaserRef, pos) {
+    this.game = game
+    this.phaserRef = phaserRef
+    this.pos = pos
+  }
+  // Tower per frame logic
+  update (game, enemies) {
+    this.aimAt(game, enemies)
+  }
+  // Implement targeting algorithm here to select target within
+  // tower attack radius
+  targetIn (enemies) {
+    // target logic
+  }
+  // Pass in game and enemy to aim the turret at
+  aimAt (game, enemy) {
+    // this.phaserRef.rotation = game.physics.arcade.angleBetween(this.phaserRef, game.input.mousePointer) + 90
+  }
+}
+
+module.exports = Tower
--- index.js
+++ index.js
@@ -9,21 +9,47 @@ const game = new Phaser.Game(
 
 let spriteSheet
 let testTower
+const playerTowers = []
+const enemies = []
+let id = 0
+let lastMouseState = 'up'
 
 function preload() {
-  // spriteSheet = game.load.spritesheet(
-  //   'towerdefense',
-  //   'assets/images/towerDefense_tilesheet.png',
-  //   16,
-  //   16,
-  //   144
-  // )
+  // You can use your own methods of making the plugin publicly available. Setting it as a global variable is the easiest solution.
+  slickUI = game.plugins.add(Phaser.Plugin.SlickUI);
+  slickUI.load('assets/ui/kenney-theme/kenney.json'); // Use the path to your kenney.json. This is the file that defines your theme.
+
   game.load.image('testTower', 'assets/images/tiles/towerDefense_tile226.png')
 }
 
 function create() {
-  testTower = game.add.sprite(16, 16, 'testTower')
   game.physics.startSystem(Phaser.Physics.ARCADE)
+  var panel;
+  slickUI.add(panel = new SlickUI.Element.Panel(8, game.height - 158, game.width - 16, 150));
+
 }
 
-function update() {}
+function update() {
+  playerTowers.forEach(tower => {
+    tower.update(game, enemies)
+  })
+  // enemies.forEach(enemy => {
+  //   enemy.update(game)
+  // })
+
+  // Drop tower on mouse click
+  if (game.input.activePointer.isDown && !mouseWasDown) {
+    dropNewTower('testTower', game.input.mousePointer.x, game.input.mousePointer.y)
+  }
+
+  // Update mouse state
+  mouseWasDown = game.input.activePointer.isDown
+}
+
+function dropNewTower(towerType, x, y) {
+  const newTower = game.add.sprite(x, y, towerType)
+  newTower.pivot.x = 64
+  newTower.pivot.y = 64
+
+  playerTowers.push(new Tower(game, newTower, { x, y }))
+}
