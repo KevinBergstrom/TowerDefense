860e24f93f5e9ff481ee09a7fc6b1f35a331eeb6
Clay Paris <clayp@rightline.com>
2017-11-29 14:53:31 -0800

    Add basic model tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
 
 describe('Model', () => {
   it('starts the wave', done => {
-    done()
     model.startWave()
     expect(model.waveStarted).to.equal(true)
+    done()
+  })
+  it('ends the wave', done => {
+    model.startWave()
+    model.endWave()
+    expect(model.waveStarted).to.equal(false)
+    done()
+  })
+  it('updates players money', done => {
+    model.money = 0
+    model.changeMoney(100)
+    expect(model.getMoney()).to.equal(100)
+    done()
+  })
+  it('takes damage', done => {
+    model.health = 100
+    model.takeDamage(50)
+    expect(model.getHealth()).to.equal(50)
+    done()
   })
 })
